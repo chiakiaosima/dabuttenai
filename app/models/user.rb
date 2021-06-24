2d71b6fead15b1d7edf582156882800fc32176b4
chiakiaosima <starsrock5937@gmail.com>
2021-06-25 07:34:03 +0900

    bootstrapの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-　
+
   with_options presence: true do
     validates :nick_name,length: { maximum: 20}
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
